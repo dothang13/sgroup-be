a536517b82b193e9e0a388abc435a0e4dd32038b
dothang13 <endiezzhang13@gmail.com>
2024-05-21 19:02:33 +0700

    validate middleware

--- app.js
+++ app.js
@@ -6,33 +6,48 @@ var app = express();
 const users = require("./users.json");
 
 app.use(bodyParser.json());
+
 const writeData = (data) => {
   fs.writeFileSync("users.json", JSON.stringify(data, null, 2));
+};
+
+const validateNewUser = (req, res, next) => {
+  const { name, email } = req.body;
+  if (!name || !email) {
+    return res.status(400).send({ message: "Name and email are required" });
+  }
+  next();
 };
 
 app.get('/api/users/', (req, res) => {
   res.send(users);
 });
 
-app.get('/api/users/:ab/:ac/', (req, res) => {
+app.get('/api/users/:id/', (req, res) => {
   const usersId = parseInt(req.params.id);
-  const user = users.find((user) =>  user.id === usersId);
-  res.send(user);
+  const user = users.find((user) => user.id === usersId);
+  if (user) {
+    res.send(user);
+  } else {
+    res.status(404).send({ message: "User not found" });
+  }
 });
 
-app.post('/api/users/', (req, res) => {
+app.post('/api/users/', validateNewUser, (req, res) => {
   const newItem = req.body;
   newItem.id = users.length ? users[users.length - 1].id + 1 : 1;
-  console.log(users);
   users.push(newItem);
   writeData(users);
-  res.status(201).send(users);
+  res.status(201).send(newItem);
 });
 
-app.put('/api/users/:id/', (req,res) =>{
+app.put('/api/users/:id/', (req, res) => {
   const updatedItem = req.body;
   const usersId = parseInt(req.params.id);
   const findIndex = users.findIndex((user) => user.id === usersId);
+  if (findIndex === -1) {
+    return res.status(404).send({ message: "User not found" });
+  }
   users[findIndex] = updatedItem;
   users[findIndex].id = usersId;
   writeData(users);
@@ -42,9 +57,12 @@ app.put('/api/users/:id/', (req,res) =>{
 app.delete("/api/users/:id/", (req, res) => {
   const usersId = parseInt(req.params.id);
   const findIndex = users.findIndex((user) => user.id === usersId);
+  if (findIndex === -1) {
+    return res.status(404).send({ message: "User not found" });
+  }
   users.splice(findIndex, 1);
   writeData(users);
-  res.send({ message: "Deleted succesfully" });
+  res.send({ message: "Deleted successfully" });
 });
 
 app.listen(3000, function () {
